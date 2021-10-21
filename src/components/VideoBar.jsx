6233c769619eea09d644ffbed921da521415bab7
aashimgarg <18bcs001@smvdu.ac.in>
2021-10-21 23:14:47 +0530

    initialised videobar

---
+++ src/components/VideoBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/VideoBar.css";
+
+const VideoBar = () => {
+    return (
+      <div className="scrollmenu">
+        <h1> this is videobar </h1>
+      </div>
+    );
+}
+export default VideoBar;
