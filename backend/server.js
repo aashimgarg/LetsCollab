fec089dac8aed47925c8fa4bc3e71363d0780967
aashimgarg <18bcs001@smvdu.ac.in>
2021-11-16 19:03:01 +0530

    added socket connection on serverSide

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ const app = express();
 const cors = require("cors");
 
 const http = require("http").Server(app);
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.use(
@@ -11,12 +19,15 @@ app.use(
   })
 );
 
-app.get('/',(req,res) => {
-  res.status(200).send("Hello World");
-})
-
-
-
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
 
 http.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
